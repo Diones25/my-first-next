a927c65674c17b647b511555d44cc982af1398c9
Diones <pereiradiones987@gmail.com>
2025-09-08 10:16:27 -0300

    Rotas dinâmicas: Pegando tudo

---
+++ src/app/shop/[...slug]/page.tsx
@@ -0,0 +1,16 @@
+
+type Props = {
+  params: {
+    slug: string[]
+  }
+}
+
+const Page = ({ params }: Props) => {
+  return (
+    <div>
+      Filtros: {params.slug.join(', ')}
+    </div>
+  )
+}
+
+export default Page;
